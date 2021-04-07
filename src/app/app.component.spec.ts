f3463505db1aa7f9b393d6d432a2886ad181cc77
Dor Shamir <dorshamir4@gmail.com>
2021-04-08 02:45:03 +0300

    Done

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Welcome to Projects Management'`, () => {
+  it(`should have as title 'Projects Management'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Welcome to Projects Management');
+    expect(app.title).toEqual('Projects Management');
   });
-
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain('Projects Management app is running!');
-  // });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GlobalService } from './service/global.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Welcome to Projects Management';
+export class AppComponent implements OnInit {
+  
+  currentProjectId!: number;
+
+  constructor(private globalService: GlobalService) { }
+
+  ngOnInit(): void {
+    this.globalService.getAllState().subscribe((state: any) => {
+      this.currentProjectId = state.currentProjectId;
+    });
+  }
+
+  title = 'Projects Management';
 }

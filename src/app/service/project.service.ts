6184a8e1899c11566c45a2714ae1813ef5e34cf9
Dor Shamir <dorshamir4@gmail.com>
2021-04-08 16:00:19 +0300

    Prepare Angular Projects Management for Heroku

--- src/app/service/project.service.ts
+++ src/app/service/project.service.ts
@@ -10,28 +10,29 @@ import { environment } from 'src/environments/environment';
 })
 export class ProjectService {
   
-  private BASE_URL = environment.baseUrl+ "projects";
+  private BASE_URL = environment.baseUrl;
+  private PROJECTS = "projects";
 
   constructor(private httpClient: HttpClient, private store: Store<any>) { }
   
   getProjectsList(): Observable<Project[]> {
-    return this.httpClient.get<Project[]>(`${this.BASE_URL}`);
+    return this.httpClient.get<Project[]>(`${this.BASE_URL}/${this.PROJECTS}`);
   }
   
   getProjectById(id: number): Observable<Project> {
-    return this.httpClient.get<Project>(`${this.BASE_URL}/${id}`);
+    return this.httpClient.get<Project>(`${this.BASE_URL}/${this.PROJECTS}/${id}`);
   }
   
   createProject(project: Project): Observable<Object> {
-    return this.httpClient.post(`${this.BASE_URL}`, project);
+    return this.httpClient.post(`${this.BASE_URL}/${this.PROJECTS}`, project);
   }
   
   updateProject(id: number, project: Project): Observable<Object> {
-    return this.httpClient.put(`${this.BASE_URL}/${id}`, project);
+    return this.httpClient.put(`${this.BASE_URL}/${this.PROJECTS}/${id}`, project);
   }
   
   deleteProject(id: number): Observable<Object> {
-    return this.httpClient.delete(`${this.BASE_URL}/${id}`);
+    return this.httpClient.delete(`${this.BASE_URL}/${this.PROJECTS}/${id}`);
   }
   
   updateProjectInState(obj: { action: any; payload: any; }) {
